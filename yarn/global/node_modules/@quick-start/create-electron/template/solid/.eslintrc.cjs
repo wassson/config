5c475098b9be0e93486f20b773b1c55978a15e5c
Austin Wasson <austinpwasson@gmail.com>
2024-01-15 10:42:14 -0700

    nvim

---
+++ yarn/global/node_modules/@quick-start/create-electron/template/solid/.eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ['solid'],
+  extends: [
+    'eslint:recommended',
+    'plugin:solid/recommended',
+    '@electron-toolkit',
+    '@electron-toolkit/eslint-config-prettier'
+  ]
+}
