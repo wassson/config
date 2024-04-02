71973536f662fc7d04b1e02eb43a803dc305b963
Austin Wasson <austinpwasson@gmail.com>
2024-04-02 13:15:55 -0600

    update telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
-  { 
-    'nvim-telescope/telescope-fzf-native.nvim', 
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
     build = 'make',
     config = function()
       require('telescope').load_extension('fzf')
@@ -9,26 +9,26 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
-    end
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-              -- even more opts
-            }
-          }
-        }
+    keys = {
+      {
+        "<leader>f",
+        function() require("telescope.builtin").find_files() end,
+        desc = "Find File",
+      },
+      {
+        "<leader>F",
+        function() require("telescope.builtin").live_grep() end,
+        desc = "Search in Files",
       }
-      require("telescope").load_extension("ui-select")
-    end
+    },
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
   }
 }
 
